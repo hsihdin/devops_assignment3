cdf0c6cf224a1c15bddb1c0b4330ca0ed8297481
hsihdin <hsihdin@users.noreply.github.com>
2024-03-18 18:32:27 -0400

    added student id

--- test_area.py
+++ test_area.py
@@ -3,7 +3,9 @@ from src.area import calculate_area_square
 
 def test_calculate_area_square():  
     assert calculate_area_square(2)==4  
-    assert calculate_area_square(3)==10  
+    assert calculate_area_square(3)==10
+    # test with last 2 digits of student id  
+    assert calculate_area_square(77)==5929
 
           
 def test_calculate_area_square_negative():  
